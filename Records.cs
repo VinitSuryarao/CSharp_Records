2632014d8de4def6b26c10aa8331ed1decf20aa7
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-01-04 14:29:32 +0530

    Add files via upload

---
+++ Records.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InitOnly
+{
+    public record Company
+    {
+        public int ID { get; init; } 
+        public string Name { get; init; }
+        public string City { get; init; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company obj = new Company
+            {
+                ID = 1, 
+                Name = "ABC", 
+                City = "Mumbai"
+            }; // Set value only when object creation
+
+            Console.WriteLine(obj.ID.ToString()+" "+obj.Name+" "+obj.City);
+            Console.ReadLine();
+        }
+    }
+}
